88f356c4276749bf63604559339cf78527580eba
ajimenezdev <ajmjimens@gmail.com>
2019-05-24 10:23:59 +0200

    utilizar Redux para operaciones CRUD en notas y categorias

--- src/screens/CategoriesScreen/index.js
+++ src/screens/CategoriesScreen/index.js
@@ -6,12 +6,20 @@ import {
   TextInput,
   Button,
   FlatList,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert
 } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import basicStyles from "ReactNativeNotas/src/styles/basicStyles";
 import { HR, ColorPicker, ColorView } from "ReactNativeNotas/src/components";
 import withColors from "ReactNativeNotas/src/components/withColors";
 import DrawerHeaderButton from "ReactNativeNotas/src/navigation/DrawerHeaderButton";
+import {
+  addCategory,
+  updateCategory,
+  removeCategory
+} from "ReactNativeNotas/src/redux/categoriesReducer";
 import CategoryItem from "./CategoryItem";
 
 const styles = StyleSheet.create({
@@ -30,36 +38,24 @@ const styles = StyleSheet.create({
   }
 });
 
-const categories = [
-  {
-    id: 1,
-    category: "Personal",
-    color: "#FFB3BA"
-  },
-  {
-    id: 2,
-    category: "Trabajo",
-    color: "#FFDEB9"
-  },
-  {
-    id: 3,
-    category: "Casa",
-    color: "#FFFFB9"
-  }
-];
-
 class CategoriesScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
-    title: "Categorías",
+    title: "Notas",
     headerLeft: <DrawerHeaderButton navigation={navigation} />
   });
 
   constructor(props) {
     super(props);
+
+    this.defaultNewCat = {
+      category: null,
+      color: props.colors.categoryColors[0]
+    };
+
     this.state = {
       modalVisible: false,
       itemSelected: null,
-      newColor: props.colors.categoryColors[0]
+      newCategory: this.defaultNewCat
     };
   }
 
@@ -71,21 +67,71 @@ class CategoriesScreen extends Component {
   };
 
   handleChangeColor = color => {
-    this.setState({ modalVisible: false, itemSelected: null });
+    const { itemSelected } = this.state;
+    if (itemSelected) {
+      // Actualizar seleccionada
+      this.props.updateCategory({
+        ...itemSelected,
+        color
+      });
+      this.setState({ modalVisible: false, itemSelected: null });
+    } else {
+      // Actualizar nueva
+      this.setState({
+        modalVisible: false,
+        newCategory: { ...this.state.newCategory, color }
+      });
+    }
+  };
 
-    // TODO: actualizar color
+  updateNewCategory = category => {
+    this.setState({ newCategory: { ...this.state.newCategory, category } });
+  };
+
+  addCategory = () => {
+    this.props.addCategory(this.state.newCategory);
+    this.setState({ newCategory: this.defaultNewCat });
+  };
+
+  updateCategoryName = (category, name) => {
+    this.props.updateCategory({
+      ...category,
+      category: name
+    });
+  };
+
+  removeCategory = category => {
+    Alert.alert(
+      "Borrar categoría",
+      `Quieres borrar '${category.category}' de la lista y de todas las notas?`,
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Borrar",
+          style: "destructive",
+          onPress: () => this.props.removeCategory(category.id)
+        }
+      ]
+    );
+    // this.props.removeCategory(category.id);
   };
 
   render() {
-    const { modalVisible, itemSelected, newColor } = this.state;
+    const { categories } = this.props;
+    const { modalVisible, itemSelected, newCategory } = this.state;
     return (
       <View style={[basicStyles.container, styles.container]}>
         <View style={styles.row}>
-          <TextInput style={styles.addInput} placeholder="Nueva categoría" />
+          <TextInput
+            style={styles.addInput}
+            placeholder="Nueva categoría"
+            value={newCategory.category}
+            onChangeText={this.updateNewCategory}
+          />
           <TouchableOpacity onPress={() => this.openChangeColor(null)}>
-            <ColorView color={newColor} />
+            <ColorView color={newCategory.color} />
           </TouchableOpacity>
-          <Button title="+" />
+          <Button title="+" onPress={this.addCategory} />
         </View>
         <HR />
         <FlatList
@@ -93,18 +139,43 @@ class CategoriesScreen extends Component {
           data={categories}
           keyExtractor={item => item.id.toString()}
           renderItem={({ item }) => (
-            <CategoryItem item={item} openChangeColor={this.openChangeColor} />
+            <CategoryItem
+              item={item}
+              openChangeColor={this.openChangeColor}
+              onChangeText={text => this.updateCategoryName(item, text)}
+              onRemove={() => this.removeCategory(item)}
+            />
           )}
           ItemSeparatorComponent={() => <HR size="100%" />}
         />
         <ColorPicker
           visible={modalVisible}
           onChange={this.handleChangeColor}
-          selectedColor={itemSelected ? itemSelected.color : newColor}
+          selectedColor={itemSelected ? itemSelected.color : newCategory.color}
         />
       </View>
     );
   }
 }
 
-export default withColors(CategoriesScreen);
+const mapStateToProps = state => {
+  return {
+    categories: state.categories
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      addCategory,
+      updateCategory,
+      removeCategory
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withColors(CategoriesScreen));
