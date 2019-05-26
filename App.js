3e7cb048da0316366b9f0037b093654913408c4e
ajimenezdev <ajmjimens@gmail.com>
2019-05-26 23:36:51 +0200

    Añadir react-native-popup-menu, para controlar opciones de notas

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, { Component } from "react";
 import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
+import { MenuProvider } from "react-native-popup-menu";
 import configureStore from "./src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import AppNavigator from "./src/navigation/AppNavigator";
@@ -23,7 +24,9 @@ export default class App extends Component<Props> {
         <StatusBar barStyle="light-content" />
         <Provider store={store}>
           <PersistGate persistor={persistor}>
-            <AppNavigator />
+            <MenuProvider>
+              <AppNavigator />
+            </MenuProvider>
           </PersistGate>
         </Provider>
       </React.Fragment>
--- src/screens/NotesScreen/NoteGridItem/index.js
+++ src/screens/NotesScreen/NoteGridItem/index.js
@@ -1,5 +1,14 @@
-import React from "react";
-import { TouchableOpacity, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger
+} from "react-native-popup-menu";
+import { removeNote } from "ReactNativeNotas/src/redux/notesReducer";
 import { Text } from "ReactNativeNotas/src/components";
 import withColors from "ReactNativeNotas/src/components/withColors";
 import getBasicStyles from "ReactNativeNotas/src/styles/basicStyles";
@@ -14,26 +23,79 @@ const styles = StyleSheet.create({
   }
 });
 
-const NoteGridItem = ({ note, category, onPress, colors }) => {
-  const { title, text } = note;
-  const basicStyles = getBasicStyles(colors);
-  return (
-    <TouchableOpacity
-      style={[
-        basicStyles.paper,
-        styles.container,
-        {
-          backgroundColor:
-            (category && colors.categoryColors[category.colorIdx]) ||
-            colors.backgroundContent
-        }
-      ]}
-      onPress={() => onPress(note)}
-    >
-      <Text style={styles.title}>{title}</Text>
-      <Text numberOfLines={5}>{text}</Text>
-    </TouchableOpacity>
+class NoteGridItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMenuOpen: false
+    };
+  }
+
+  editFromMenu = note => {
+    this.setState({ isMenuOpen: false });
+    this.props.onPress(note);
+  };
+
+  deleteFromMenu = note => {
+    this.props.removeNote(note.id);
+  };
+
+  render() {
+    const { note, category, onPress, colors } = this.props;
+    const { title, text } = note;
+    const basicStyles = getBasicStyles(colors);
+    const { isMenuOpen } = this.state;
+    return (
+      <TouchableOpacity
+        style={[
+          basicStyles.paper,
+          styles.container,
+          {
+            backgroundColor:
+              (category && colors.categoryColors[category.colorIdx]) ||
+              colors.backgroundContent
+          }
+        ]}
+        onPress={() => onPress(note)}
+        onLongPress={() => this.setState({ isMenuOpen: true })}
+      >
+        <Menu opened={isMenuOpen}>
+          <MenuTrigger />
+          <MenuOptions opened={isMenuOpen}>
+            <MenuOption
+              triggerOnLongPress
+              onSelect={() => this.editFromMenu(note)}
+              text={"Editar"}
+            />
+            <View style={styles.divider} />
+            <MenuOption
+              onSelect={() => this.deleteFromMenu(note)}
+              text="Borrar"
+            />
+          </MenuOptions>
+        </Menu>
+        <Text style={styles.title}>{title}</Text>
+        <Text numberOfLines={5}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      removeNote
+    },
+    dispatch
   );
 };
 
-export default withColors(NoteGridItem);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withColors(NoteGridItem));
