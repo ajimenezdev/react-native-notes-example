8fa41ff15b32df5438fa8c0ddcc6498195bb2749
ajimenezdev <ajmjimens@gmail.com>
2019-05-31 21:10:07 +0200

    Añadir Firebase Notifications depencies para android

--- android/app/src/main/java/com/reactnativenotas/MainApplication.java
+++ android/app/src/main/java/com/reactnativenotas/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(), new RNFirebasePackage(), new RNGestureHandlerPackage(),
-          new VectorIconsPackage(), new RNFirebaseAuthPackage(), new RNFirebaseFirestorePackage());
+          new VectorIconsPackage(), new RNFirebaseAuthPackage(), new RNFirebaseFirestorePackage(),
+          new RNFirebaseNotificationsPackage());
     }
 
     @Override
